2ae17e83d72bb1c66bce7907536385e858cbfda5
Javier Benegas <javier.benegas@hotmail.com>
2022-06-07 22:12:06 -0300

    nueva carga DOM

---
+++ DOM.js
@@ -0,0 +1,10 @@
+//Crear elemento
+let titulo = document.getElementById("titulo")
+console.log(titulo.innerText)  //"Calcula tu cuota"
+titulo.innerText = "Calcula el valor de tu cuota"
+
+
+//Eliminar elemento
+let eliminar = document.getElementById("titulo_bienvenida")
+eliminar.remove();
+
